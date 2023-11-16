c8c8a6d2e85c931d29e4679ae307fbe96c595ca5
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-16 16:54:08 +0300

    feat: complete task4 module4.2 v.7

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -9,15 +9,15 @@ public class TwoRangesSum {
             int sumCount = 0;
             int counter = 0;
             do {
-                if (counter % numberToSkip == 0) {
+                if (counter <= numberToSkip) {
                     sumSkip += counter;
                 } else {
                     sumCount += counter;
                 }
                 counter++;
-            } while (numberToSkip < lastInRow && counter < lastInRow);
+            } while (numberToSkip <= lastInRow && counter <= lastInRow);
             if (numberToSkip > lastInRow) {
-                System.out.println("number to skip is bugger then the last");
+                System.out.println("number to skip is bigger then the last");
             } else {
                 System.out.println("skipped sum is " + sumSkip);
                 System.out.println("counted sum is " + sumCount);
