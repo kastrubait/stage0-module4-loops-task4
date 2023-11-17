36712ffff30a7dd5999d019599f2a1d828eb7927
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-17 09:48:18 +0300

    feat: complete task4 module4.2 v.9

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,14 +3,11 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
         if (toBreakWith <= numberToGoUntil) {
-            for (int i = 1; i <= toBreakWith; i++ ) {
-                System.out.println(i);
-            }
-        } else {
-            for (int i = 1; i <= numberToGoUntil; i++ ) {
-                System.out.println(i);
-            }
+            System.out.println("iterating till the end");
         }
-
+        for (int i = 1; i <= numberToGoUntil; i++ ) {
+            if (toBreakWith == i) break;
+            System.out.println(i);
+        }
     }
 }
